7e2191bfffedc4c88f34d3b4b2ee9ba1325e5bd8
ehroy <ahmadroy092@gmail.com>
2026-04-19 12:32:16 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		port: 4174,
-		strictPort: true
-	},
-	preview: {
-		port: 4175,
-		strictPort: true
-	}
+  plugins: [sveltekit()],
+  server: {
+    proxy: {
+      "/api": { target: "http://localhost:9001", changeOrigin: true },
+    },
+    port: 4174,
+    strictPort: true,
+  },
+  preview: {
+    port: 4175,
+    strictPort: true,
+  },
 });
